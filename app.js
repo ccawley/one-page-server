fe2a14999d47ba5a85b9b540ceec241c7eced26d
Curtis Cawley <curtis.cawley@icloud.com>
2018-01-09 13:46:47 -0800

    set up server practice, added routes

--- app.js
+++ app.js
@@ -2,5 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const listener = () => console.log(`Application listening on port ${port}!`)
+const listener = () => console.log(`Heckin' dang doggos party on port ${port}!`)
 app.listen(port, listener)
+
+app.get('/dogs', (req, res) => {
+  const message = 'WOOF!'
+  console.log(`I AM SERVER, BORK, ${message}`)
+  res.send(`RESPONSE TO CLIENT, BORK ${message}`)
+})
+
+app.get('/ping', (req, res) => {
+  const message = 'pong!'
+  console.log(`ON THE SERVER: ${message}`)
+  res.setHeader('content-type', 'text/html');
+  res.send(`<h1>Now you're cooking with oil!</h1>`)
+})
